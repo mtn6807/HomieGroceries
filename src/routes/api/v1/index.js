c12eed9aacd49a7e5a7e94fbea16f99a1739d613
michael <mtn6807@rit.edu>
2020-07-14 18:13:57 -0400

    added base register endpoint

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,3 +1,6 @@
+const config = require('/../../../config');
+const users = require('../../../controller/user.js');
+
 var express = require('express');
 var router = express.Router();
 
@@ -5,6 +8,21 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 	// return a list of endpoints
 	res.send('Sorry, no endpoints yet!');
+});
+
+/* 
+* POST /api/v1/register/
+* endpoint to register users.
+*
+*{username: <username>, password: <password>}
+*/
+router.post('/register', (req, res) => {
+	try{
+		users.addUser(req.username,req.password);
+	i	res.status(200).json({status:"ok"});
+	}catch(err){
+		res.status(503);
+	}
 });
 
 module.exports = router;
