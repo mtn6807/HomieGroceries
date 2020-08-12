dec38c314b8c921e86846529976f5f15aa9852b2
Michael Nee <michaelnee@Michaels-MacBook-Pro.local>
2020-08-12 19:58:46 -0400

    passwords being hashed

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,16 +2,22 @@ const config = require('../config');
 const dbString = 'mongodb://'+config.dbURI+':'+config.dbPort+'/users';
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
+const bcrypt = require('bcrypt');
+const saltRounds = 3;
 
 function addUser(username, password, mail){
 	MongoClient.connect(dbString, (err, client)=>{
 		var db = client.db('homies');
 		if(err){throw err};
-		var doc = {user: username, pass: password, email: mail};
 
-		db.collection('users').insertOne(doc, (err,res)=>{
-			if(err){throw err};
-			client.close();
+		bcrypt.genSalt(saltRounds, (err, salt)=>{
+			bcrypt.hash(password, salt, (err, hash)=>{
+				var doc = {user: username, pass: hash, email: mail};
+				db.collection('users').insertOne(doc, (err,res)=>{
+					if(err){throw err};
+					client.close();
+				});
+			});
 		});
 	});
 }
