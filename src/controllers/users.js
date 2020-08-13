939bf5c4276983d31aac5178b8a95ba8b63928c7
Michael Nee <michaelnee@Michaels-MacBook-Pro.local>
2020-08-12 21:59:15 -0400

    added userdedupe

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -5,22 +5,59 @@ const MongoClient = mongodb.MongoClient;
 const bcrypt = require('bcrypt');
 const saltRounds = 3;
 
-function addUser(username, password, mail){
-	MongoClient.connect(dbString, (err, client)=>{
-		var db = client.db('homies');
-		if(err){throw err};
-
-		bcrypt.genSalt(saltRounds, (err, salt)=>{
-			bcrypt.hash(password, salt, (err, hash)=>{
-				var doc = {user: username, pass: hash, email: mail};
-				db.collection('users').insertOne(doc, (err,res)=>{
-					if(err){throw err};
-					client.close();
-				});
-			});
+function userExists(username){
+	return new Promise((resolve, reject)=>{
+		MongoClient.connect(dbString, (err, client)=>{
+			if(err){throw err;}
+			let foundOurBoy = false;
+			const db = client.db('homies');
+			db.collection('users').find({user:{$exists:true,$in:[username]}}).forEach(result=>{
+				foundOurBoy = true;
+			}).then(()=>{resolve(foundOurBoy)});
 		});
 	});
 }
+
+function emailExists(email){
+	return new Promise((resolve, reject)=>{
+		MongoClient.connect(dbString, (err, client)=>{
+			if(err){throw err;}
+			let foundOurBoy = false;
+			const db = client.db('homies');
+			db.collection('users').find({email:{$exists:true,$in:[email]}}).forEach(result=>{
+				foundOurBoy = true;
+			}).then(()=>{resolve(foundOurBoy)});
+		});
+	});
+}
+
+async function addUser(username, password, mail){
+	return new Promise((resolve,reject)=>{
+		MongoClient.connect(dbString, async(err, client)=>{
+			var db = client.db('homies');
+			if(err){throw err};
+
+			let userexisting = await userExists(username)
+			if(userexisting){resolve(1);client.close();return;}
+
+			let emailexisting = await emailExists(mail)
+			if(emailexisting){resolve(2);client.close();return;}
+
+			bcrypt.genSalt(saltRounds, (err, salt)=>{
+				bcrypt.hash(password, salt, (err, hash)=>{
+					var doc = {user: username, pass: hash, email: mail};
+					db.collection('users').insertOne(doc, (err,res)=>{
+						if(err){throw err};
+						client.close();
+						resolve(0);
+					});
+				});
+			});
+		})
+	});
+}
+
+
 
 // module.export(addUser);
 
--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -16,10 +16,13 @@ router.get('/', function(req, res, next) {
 *
 *{username: <username>, password: <password>, email: <email>}}
 */
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
 	try{
-		users.addUser(req.body.uname,req.body.pword,req.body.email);
-		res.status(200).json({status:"ok"});
+		const valid = await users.addUser(req.body.uname,req.body.pword,req.body.email);
+		console.log(valid)
+		if(valid==0){res.status(200).json({status:"ok"});}
+		if(valid==1){res.status(409).json({status:"conflict: username in use"});}
+		if(valid==2){res.status(409).json({status:"conflict: email in use"});}
 	}catch(err){
 		res.status(503);
 	}
